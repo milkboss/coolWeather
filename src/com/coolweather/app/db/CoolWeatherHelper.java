9068a792ee224e7aeb3172172bc96ca21f73fc6b
TOM <zj269512@icloud.com>
2016-08-17 14:04:41 +0800

    完成遍历省市县三级列表功能.

--- src/com/coolweather/app/db/CoolWeatherHelper.java
+++ src/com/coolweather/app/db/CoolWeatherHelper.java
@@ -16,14 +16,14 @@ public class CoolWeatherHelper  extends SQLiteOpenHelper {
 	 */
 	public static final String CREATE_PROVINCE="create table Province("
 			+ "id integer primary key autoincrement, "
-			+"province_name text. "
-			+"provincee_code text) ";
+			+"province_name text, "
+			+"province_code text) ";
 	
 	/**
 	 * City±ÌΩ®±Ì”Ôæ‰
 	 */
 	public static final String CREATE_CITY="create table City("
-			+"id integer primary key autoincrement. "
+			+"id integer primary key autoincrement, "
 			+ "city_name text, "
 			+ "city_code text, "
 			+ "province_id integer)";
---
+++ src/com/coolweather/app/util/Utility.java
@@ -0,0 +1,88 @@
+package com.coolweather.app.util;
+
+import com.coolweather.app.db.CoolWeatherDB;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+import android.text.TextUtils;
+
+public class Utility {
+
+ /**
+ * @param coolWeatherDB
+ * @param response
+ * 解析和处理服务器返回的省级数据
+ */
+public synchronized static boolean handlerProvinceResponse(
+CoolWeatherDB coolWeatherDB, String response){
+	if(!TextUtils.isEmpty(response)){
+String[] allProvinces=response.split(",");
+if(allProvinces!=null&&allProvinces.length>0){
+for(String p:allProvinces){
+	String[]array=p.split("\\|");
+	Province province=new Province();
+	province.setProvinceCode(array[0]);
+	province.setProvinceName(array[1]);
+	//将解析出来的数据存储到Province表中
+	coolWeatherDB.saveProvince(province);
+}
+return true;
+}
+	}
+	
+	return false;
+ }
+
+
+
+/**
+ * @param coolweatherDB
+ * @param response
+ * @param provinceId
+ * 处理服务器返回的市级数据
+ */
+public static boolean handlerCitiesResponse(
+CoolWeatherDB coolweatherDB,String response,int provinceId){
+	if(!TextUtils.isEmpty(response)){
+String[] allCities=response.split(",");
+if(allCities!=null&allCities.length>0){
+	for(String c:allCities){
+		String[]array=c.split("\\|");
+		City city=new City();
+		city.setCityCode(array[0]);
+		city.setCityName(array[1]);
+		city.setProvinceId(provinceId);
+		//将解析处理的数据存储到City表中
+		coolweatherDB.saveCity(city);
+	}
+	return true;
+}
+	}
+	return false;
+}
+
+public static boolean handleCountiesResponse(
+CoolWeatherDB coolWeatherDB,String response,int cityId){
+	if(!TextUtils.isEmpty(response)){
+		String[]allCounties=response.split(",");
+if(allCounties!=null&allCounties.length>0){
+	for(String c:allCounties){
+String[] array=c.split("\\|");
+County county=new County();
+county.setCountyCode(array[0]);
+county.setCountyName(array[1]);
+county.setCityId(cityId);
+//将解析处理的数据存储到County表中
+coolWeatherDB.saveCounty(county);
+	}
+	return true;
+}
+	}
+	return false;
+}
+
+
+
+
+}
